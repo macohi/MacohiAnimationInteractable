8ffa35c6a3efb528a696e81956b0bac41109eeb2
Macohi <sinconsistencia@gmail.com>
2026-02-13 12:19:30 -0600

    medals save field

--- source/MAISave.hx
+++ source/MAISave.hx
@@ -1,12 +1,22 @@
+import macohi.save.SaveField;
 import macohi.save.Save;
 
 class MAISave extends Save
 {
+	public var medals:SaveField<Array<String>>;
+
 	override public function new()
 	{
 		super();
 
 		SAVE_VERSION = 1;
 		init('MacohiAnimationInteractable', null);
+	}
+
+	override function initFields()
+	{
+		super.initFields();
+
+		medals = new SaveField<Array<String>>('medals', [], 'Medals');
 	}
 }
