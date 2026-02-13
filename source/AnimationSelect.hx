9450bc7a06d732ac7601fd3b40682430a5d93e62
Macohi <sinconsistencia@gmail.com>
2026-02-13 08:06:06 -0600

    controls

--- source/AnimationSelect.hx
+++ source/AnimationSelect.hx
@@ -1,3 +1,4 @@
+import flixel.FlxG;
 import macohi.funkin.koya.frontend.scenes.menustates.OptionsMenuState;
 
 class AnimationSelect extends OptionsMenuState
@@ -6,7 +7,25 @@ class AnimationSelect extends OptionsMenuState
 	{
 		super();
 		this.atlasText = false;
+	}
 
-		this.itemList = [];
+	override function controlsMoveVertical()
+	{
+		super.controlsMoveVertical();
+
+		if (FlxG.keys.anyJustReleased([W, UP]))
+			select(-1);
+		if (FlxG.keys.anyJustReleased([S, DOWN]))
+			select(1);
+	}
+
+	override function controlsOther() {
+		super.controlsOther();
+
+		if (FlxG.keys.anyJustReleased([ENTER]))
+			acceptFunction();
+		
+		// if (FlxG.keys.anyJustReleased([ESCAPE]))
+			// acceptFunction();
 	}
 }
